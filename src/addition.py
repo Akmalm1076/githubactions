238a4af5f67d335ec3a2bc1deaa9cd8c9e0840a7
Akmalm1076 <akmalmansari1076@gmail.com>
2026-09-07 15:53:54 +0530

    Clean up addition.py
    Removed unused test function and added a comment.

--- src/addition.py
+++ src/addition.py
@@ -1,10 +1,9 @@
 # app.py
 # This is a test commit
+# Some change
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
-def test_oomb():
-    return "Alan jose periera is prithviraj suku"
